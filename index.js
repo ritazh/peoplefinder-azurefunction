5c7fbcd0a91de2aa4e8f054bb792361777454a48
Rita Zhang <rita.z.zhang@gmail.com>
2016-10-26 11:50:34 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+var request = require('request');
+
+module.exports = function (context, myTimer) {
+    var timeStamp = new Date().toISOString();
+    context.log(timeStamp);
+    
+    if(myTimer.isPastDue)
+    {
+        context.log('Node.js is running late!');
+    }
+    context.log('Node.js timer trigger function ran!', timeStamp); 
+
+    request.post({
+        url: 'https://dosttdemo.azurewebsites.net/parse/push',
+        headers: {
+            'X-Parse-Application-Id': process.env.APP_ID,
+            'X-Parse-Master-Key': process.env.MASTER_KEY,
+            'Content-Type': 'application/json'
+        },
+        body: {
+            'where': { 
+                "userID": process.env.testUserID
+            }, 
+            'data': { 
+                'alert': 'Testing push from Azure Function'
+            }
+        }
+    }, function(err, resp) {
+        if(err) context.log(err);
+        try {
+            context.log('got resp');
+            context.log(resp);
+        } catch(e) {
+            context.log(e);
+        } finally {
+            context.done();
+        }
+  });
+    
+};
