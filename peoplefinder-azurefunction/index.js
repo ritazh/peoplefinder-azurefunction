bd9f0bc99be85b170f00833d820b1ae737bb636a
Rita Zhang <rita.z.zhang@gmail.com>
2016-10-26 23:09:42 -0700

    Add query for people with same interests

--- peoplefinder-azurefunction/index.js
+++ peoplefinder-azurefunction/index.js
@@ -16,47 +16,89 @@ module.exports = function (context, inputQueueItem) {
   context.log(inputQueueItem);
 
   var user = inputQueueItem;
-  ///TODO: Find match for this user
+  
+  mongo.connect(connectionString, function (err, db) {
+    assert.equal(null, err);
+    var users = db.collection('_User');
+    users.find({ username: user }, { Interests: 1, _id: 0 }).toArray(function (err, docs) {
+        context.log(docs.length);
+        context.log(JSON.stringify(docs));
 
+        if (docs.length > 0){
+          var userInterests = docs[0].Interests;
+          context.log(userInterests);
+          context.log('before aggregate');
 
-  var msg = 'Found a match for you, ' + user;
+          users.aggregate([
+              { $match: { Interests: { $in: userInterests } } },
+              {
+                  $project: {
+                    "username": 1,
+                    "screenName": 1,
+                    "matches":
+                    { $setIntersection: [userInterests, "$Interests"] },
+                    "matchSize": { $size: { $setIntersection: [userInterests, "$Interests"] } }
 
-  if (user){
-    request.post({
-      url: process.env.PARSEPUSHURL,
-      headers: {
-          'X-Parse-Application-Id': process.env.APP_ID,
-          'X-Parse-Master-Key': process.env.MASTER_KEY,
-          'Content-Type': 'application/json'
-      },
-      json: {
-          'where': { 
-              "userID": process.env.testUserID
-          }, 
-          'data': { 
-              'alert': msg
-          }
-      }
-    }, function(err, resp) {
-        context.log('got resp');
-        if(err) context.log(err);
-        try {
-            //context.log(resp);
-        } catch(e) {
-            context.log(e);
-        } finally {
-            context.log('sending SMS...');
+                  }
+              },
+            { $sort: { matchSize: -1 } },
+            { $limit: 2 }
+          ]).toArray(function (err, matchdocs) {
+              context.log('after get matchdocs');
+              context.log(matchdocs);
+              context.log(matchdocs.length);
+              
+              var msg = "We've found a match for you, " + user + '!';
+              context.log(msg);
+              context.done();
+              db.close();
+            });
 
-            context.bindings.message = {
-                body: msg,
-                to: process.env.TWILIO_TO,
-                from: process.env.TWILIO_FROM
-            };
-            context.done();
+        }else{
+          context.log('user has no interests');
+          context.done();
+          db.close();
         }
     });
-  }else{
-    context.done();
-  }
-    
+  });
+
+  //var msg = 'Found a match for you, ' + user;
+
+  // if (user){
+  //   request.post({
+  //     url: process.env.PARSEPUSHURL,
+  //     headers: {
+  //         'X-Parse-Application-Id': process.env.APP_ID,
+  //         'X-Parse-Master-Key': process.env.MASTER_KEY,
+  //         'Content-Type': 'application/json'
+  //     },
+  //     json: {
+  //         'where': { 
+  //             "userID": process.env.testUserID
+  //         }, 
+  //         'data': { 
+  //             'alert': msg
+  //         }
+  //     }
+  //   }, function(err, resp) {
+  //       context.log('got resp');
+  //       if(err) context.log(err);
+  //       try {
+  //           //context.log(resp);
+  //       } catch(e) {
+  //           context.log(e);
+  //       } finally {
+  //           context.log('sending SMS...');
+
+  //           context.bindings.message = {
+  //               body: msg,
+  //               to: process.env.TWILIO_TO,
+  //               from: process.env.TWILIO_FROM
+  //           };
+  //           context.done();
+  //       }
+  //   });
+  // }else{
+  //   context.done();
+  // }
 };
